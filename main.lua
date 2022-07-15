8073603a05517a65191359feeed467d47cf3fd92
Erik Biederstadt <biederstadterik@gmail.com>
2022-07-15 14:38:10 -0600

    Add enemy movement and appearance

--- main.lua
+++ main.lua
@@ -1,3 +1,5 @@
+require 'utils'
+
 function love.load()
   areaWidth = 800
   arenaHeight = 600
@@ -7,8 +9,8 @@ function love.load()
     speed_x = 0,
     speed_y = 0,
 
-    -- Start in the middle of the screen (TODO: We should change this)
-    x = areaWidth / 2,
+    -- Start in the middle of the screen
+    x = 0,
     y = arenaHeight / 2
   }
 
@@ -16,11 +18,21 @@ function love.load()
   bullet_timer_limit = 0.5
   bullet_timer = bullet_timer_limit
   bullets = {}
+
+  -- TODO: This can be one of the things that I set randomly
+  enemy_timer_limit = 1.0
+  enemy_timer = 0
+  enemies = {{
+      x = areaWidth,
+      y = love.math.random(arenaHeight)
+    }}
 end
 
 function love.update(dt)
   -- TODO: This will be set randomly
   local shipSpeed = 100
+  -- TODO: It could be nice to let the users choose their controls (advanced feature)
+  -- Move the player
   if love.keyboard.isDown('w') then
     ship.speed_y = ship.speed_y - shipSpeed * dt
   end
@@ -34,7 +46,7 @@ function love.update(dt)
     ship.speed_x = ship.speed_x + shipSpeed * dt
   end
 
-  -- Update the bullet timer
+  -- Shoot bullets
   bullet_timer = bullet_timer + dt
 
   if love.keyboard.isDown('space') then
@@ -65,13 +77,34 @@ function love.update(dt)
   end
 
   -- Move the bullets, and remove them after the reach the edge of the screen
+  -- TODO: This might be something that we want to randomize
+  local bullet_speed = 500
   for bullet_index = #bullets, 1, -1 do
     local bullet = bullets[bullet_index]
-    -- TODO: This might be something that we want to randomize
-    local bullet_speed = 500
     bullet.x = bullet.x + bullet_speed * dt
     if bullet.x > areaWidth then
       table.remove(bullets, bullet_index)
+    end
+  end
+
+  -- Add new enemies
+  enemy_timer = enemy_timer + dt
+  if enemy_timer >= enemy_timer_limit then
+    table.insert(enemies, {
+      x = areaWidth,
+      y = love.math.random(arenaHeight)
+    })
+    enemy_timer = 0
+  end
+
+  -- Move the enemies
+  -- TODO: Set this randomly
+  local enemy_speed = 200
+  for enemy_index = #enemies, 1, -1 do
+    local enemy = enemies[enemy_index]
+    enemy.x = enemy.x - enemy_speed * dt
+    if enemy.x < 0 then
+      table.remove(enemies, enemy_index)
     end
   end
 end
@@ -85,5 +118,11 @@ function love.draw()
   for bullet_index, bullet in ipairs(bullets) do
     love.graphics.setColor(0, 1, 0)
     love.graphics.circle('fill', bullet.x, bullet.y, 5)
+  end
+
+  -- Draw the enemies
+  for enemy_index, enemy in ipairs(enemies) do
+    love.graphics.setColor(1, 0, 0)
+    love.graphics.circle('fill', enemy.x, enemy.y, 40)
   end
 end
